8395507d22eba12cd781c55cfca0c64bbf43b2de
kazutotakeuchi32 <kazutotakeuchi32@gmail.com>
2021-04-04 15:16:53 +0900

    get train routes

--- main.py
+++ main.py
@@ -71,12 +71,12 @@ def get_train_routes(start_station,end_station):
         time_str = ""
         for j in range(len(time)):
           time_str += time[j].get_text()
-          send_texts.append("{}{}\n".format(station[i].select_one("dl  dt").get_text().strip(),time_str.strip()))
+          send_texts.append("\n{}{}".format(station[i].select_one("dl  dt").get_text().strip(),time_str.strip()))
       far=fare.select("div ul li.platform")
       for k in range(len(fare_section)):
         send_texts[k]=send_texts[k]+re.sub("(\[train\]||\[walk\])","",fare_section[k].get_text())
       for l in range(len(far)):
-        send_texts[l]="\n"+send_texts[l]+far[l].get_text()
+        send_texts[l]=send_texts[l]+far[l].get_text()
       send_text=""
       for i in range(len(send_texts)):
         send_text+=send_texts[i]
