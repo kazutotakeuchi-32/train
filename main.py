f8437e958b529a5acfcdd699ed55b5e95ec91999
kazutotakeuchi32 <kazutotakeuchi32@gmail.com>
2021-04-06 20:16:05 +0900

    get train routes

--- main.py
+++ main.py
@@ -192,9 +192,10 @@ def handler_message(event):
       output=""
       for j in range(len(titles)):
         output+= "{}{}".format(titles[j],contents[j])
+      text="------------------------\n{}\n{}\n-----------------------".format(station_name,output)
       line_bot_api.reply_message(
         event.reply_token,
-        TextMessage(text="------------------------\n{}\n{}\n-----------------------".format(station_name,output))
+        TextMessage(text=text)
       )
 
   else :
